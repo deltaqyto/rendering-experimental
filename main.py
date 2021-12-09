ab7b64bc15fcc917c1b1ac2e8386f86e2fce5b58
Delta <61540362+deltaqyto@users.noreply.github.com>
2021-12-09 23:52:19 +1100

    Improved attribute parser + cleanup
    Replaced random exception names with more descriptive phrases
    Added edge triggering to the pause functionality
    Fixes to mixer behavior
    Comments and code cleanup
    Fixed error where list_attributes would cause a crash

--- main.py
+++ main.py
@@ -3,39 +3,77 @@ import yaml
 import bindings as gl
 
 
-def parse_attribute_functions(attribs, eval_vals, shared_data, debug=False):
+# Current priorities, in order:
+# todo implement checks for vector returns + test case
+# Todo enable scene rendering + proper attribute handling
+
+
+def parse_attribute_functions(attribs, eval_vals, shared_data):
+    def printreturn(value):  # Handy function for the print lambda
+        print(value)
+        return value
+
     if not attribs:
         return {}
-    mappings = {"const": lambda _, vals: vals[0], "linear_frames": lambda evals, vals: vals[0] * evals.get("frames", 1),
-                "const_vec3": lambda _, vals: vals[:3], "sin": lambda evals, vals: vals[1] * math.sin(vals[0] * evals.get("frames", 1)),
-                "sin_offset": lambda evals, vals: vals[1] * math.sin(vals[2] + vals[0] * evals.get("frames", 1))}
     new_attribs = {}
-    for name, attrib in attribs.items():
+
+    # Lambda takes 4 inputs. A list of stack values, a list of provided constants, the dict of evaluators and the dict of shared data
+    method_lookup = {"const": [0, 1, lambda v, c, e, s: c[0]], "multiply": [1, 1, lambda v, c, e, s: c[0] * v[0]],
+                     "frame": [0, 0, lambda v, c, e, s: e["frames"]], "add": [1, 1, lambda v, c, e, s: c[0] + v[0]],
+                     "sine": [1, 0, lambda v, c, e, s: math.sin(v[0])], "floor": [1, 1, lambda v, c, e, s: c[0] * math.floor(v[0] / c[0])],
+                     "lookup": [2, 0, lambda v, c, e, s: s.get(c[0], {}).get(c[1]) if s.get(c[0]).get(c[1]) is not None else c[2]],
+                     "fsine": [0, 2, lambda v, c, e, s: c[1] * math.sin(c[0] * e["frames"])], "abs": [1, 0, lambda v, c, e, s: math.fabs(v[0])],
+                     "print": [1, 0, lambda v, c, e, s: printreturn(v[0])]}
+
+    for name, attr in attribs.items():
+        # Handle trivial case where no command group is provided
+        if not isinstance(attr, (list, tuple)):
+            new_attribs[name] = attr
+            continue
+        # Handle case where only one command is provided
+        if not isinstance(attr[0], (list, tuple)):
+            new_attribs[name] = method_lookup[attr[0]][2]([], attr[1:], eval_vals, shared_data)
+            continue
+
+        # Check for vector count
         try:
-            temp = mappings.get(attrib[0])
-        except TypeError:
-            new_attribs[name] = attrib
-            continue
-        if temp is None:
-            continue
-        attrib = temp(eval_vals, attrib[1:])
-        new_attribs[name] = attrib
-    if debug is True:
-        print(attribs, eval_vals, new_attribs)
+            count = int(attr[0][0])
+            instructions = attr[0][1:]
+        except ValueError:
+            count = 1
+            instructions = attr[0]
+
+        output = []
+        values = []
+        constants = attr[1:] if len(attr) > 1 else []
+        for index in range(count):
+            for instruction in instructions:
+                method = method_lookup[instruction]
+                pass_consts = constants[:method[1]]
+                constants = constants[method[1]:]
+                values.append(method[2]([values.pop() for _ in range(method[0])], pass_consts, eval_vals, shared_data))
+            output.append(values[-1])
+
+        # Save output
+        if count == 1:
+            new_attribs[name] = output[0]
+        else:
+            new_attribs[name] = output
+
     return new_attribs
 
 
 def mix_attributes(attrib_set_1, attrib_set_2, default_attrs):
     """Each name in default_attrs is returned with a value derived from attrib sets 1 and 2.
     Mix behaviors allows selection of the mix type for different names"""
-    mix_behaviours = {"alpha": "mult", "pos_x": "add", "pos_y": "add", "visible": "inherit", "unused": "ignore"}   # Should "inherit" by default
+    mix_behaviours = {"alpha": "mult", "pos_x": "add", "pos_y": "add", "visible": "inherit", "unused": "ignore"}  # Should "inherit" by default
     out_attrs = {}
     for name, attr in default_attrs.items():
         mode = mix_behaviours.get(name, "inherit")
         if mode == "mult":
-            out_attrs[name] = attrib_set_1[name] * attrib_set_2[name] if name in attrib_set_1 and name in attrib_set_2 else attr[0]
+            out_attrs[name] = attrib_set_1.get(name, 1) * attrib_set_2.get(name, 1) if name in attrib_set_1 or name in attrib_set_2 else attr[0]
         elif mode == "add":
-            out_attrs[name] = attrib_set_1[name] + attrib_set_2[name] if name in attrib_set_1 and name in attrib_set_2 else attr[0]
+            out_attrs[name] = attrib_set_1.get(name, 0) + attrib_set_2.get(name, 0) if name in attrib_set_1 or name in attrib_set_2 else attr[0]
 
         elif mode == "inherit":
             out_attrs[name] = attr[0] if name not in attrib_set_1 else attrib_set_1[name]
@@ -65,12 +103,12 @@ class Object:
         raise NotImplementedError()
 
     def __repr__(self):
-        return f"Generic object {self.name}"
+        return f"{self.type} object {self.name}"
 
     def list_attributes(self):
         print(f"{self.type} object: {self.name}")
         for name, attr in self.attributes.items():
-            print(f"{name}, assume {attr[1]}, {attr[2]}")
+            print(f"{name}, assume {attr[0]}, {attr[1]}")
         print()
 
 
@@ -86,16 +124,12 @@ class RectObject(Object):
         self.vao.add_vbo(self.vbo)
 
         self.default_attrs = {"size_x": [0.5, "horizontal scale factor"], "size_y": [0.5, "vertical scale factor"],
-                           "pos_x": [0, "offset from center along x"], "pos_y": [0, "offset from center along y"],
-                           "color": [(1, 0, 1), "primary color of shape"]}
-        self.type = "Rect"
-
-    def __repr__(self):
-        return f"Rectangle object {self.name}"
+                              "pos_x": [0, "offset from center along x"], "pos_y": [0, "offset from center along y"],
+                              "color": [(1, 0, 1), "primary color of shape"]}
+        self.type = "Rectangle"
 
     def draw(self, draw_attrs):
-
-        # line_weight, radius
+        # todo add line_weight, radius
         matrix = gl.Mat4(1.0)
         matrix = gl.translate(matrix, gl.Vec3(draw_attrs.get("pos_x"), draw_attrs.get("pos_y"), 0))
         matrix = gl.scale(matrix, gl.Vec3(draw_attrs.get("size_x"), draw_attrs.get("size_y"), 1))
@@ -123,7 +157,6 @@ class RegPoly(Object):
 
         self.vertices = [0 for _ in range(self.default_attrs["max_faces"][0] * 2 + 2)]
         self.indices = [0 for _ in range(self.default_attrs["max_faces"][0] * 3)]
-        print(self.default_attrs["faces"])
 
         self.vbo.add_data(self.vertices, gl.GL_const.dynamic_draw)
         self.ebo.add_data(self.indices, gl.GL_const.dynamic_draw)
@@ -131,46 +164,50 @@ class RegPoly(Object):
         self.vao.add_vbo(self.vbo)
         self.vao.add_ebo(self.ebo)
 
-        self.type = "RegPolygon"
-
-    def __repr__(self):
-        return f"Regular polygon object {self.name}"
+        self.type = "Regular Polygon"
 
     def distribute(self, a, b, num):
         """Get intersection point of a line y = mx and ellipse x^2/a^2 + y^2/b^2 = 1, for num points spaced with equal separation angles """
         out_points = []
+        num = round(num)
+
+        if a == 0 or b == 0 or num == 0:  # Catch division by zero and trivial cases
+            return out_points
+
         divisor = 2 * math.pi / num
         for cur in range(num):
             m = divisor * cur
-            if m == math.pi / 2:
+
+            if m == math.pi / 2:  # Catch cases where tan(m) would be infinite
                 out_points += [0, b]
                 continue
-            if m == 3 * math.pi/2:
+            if m == 3 * math.pi / 2:
                 out_points += [0, -b]
                 continue
-            multiplier = 1 if 0 <= m < math.pi/2 or 3 * math.pi/2 < m <= 2 * math.pi else -1
+
+            # Decide which side of the origin the intersection is
+            multiplier = 1 if 0 <= m < math.pi / 2 or 3 * math.pi / 2 < m <= 2 * math.pi else -1
+
             m = math.tan(m)
-            intersection = [((a*a * b*b)/(a*a * m*m + b*b)) ** 0.5 * multiplier, 0]
-            intersection[1] = m * intersection[0]
+            intersection = [((a * a * b * b) / (a * a * m * m + b * b)) ** 0.5 * multiplier, 0]
+            intersection[1] = m * intersection[0]  # Get y coord of the intersect y = mx
             out_points += intersection
         return out_points
 
-        #todo add catch case where gradient is infinity, in which case intersection is (0, +-b)
-
     def tri_fan_indices(self, num):
+        """Generate indices list for a triangle fan with num faces"""
         output = []
-        for count in range(num):
+        for count in range(round(num)):
             output += [0, count + 1, count + 2]
         if output:
             output[-1] = 1
         return output
-            
+
     def __del__(self):
         self.ebo.free()
         super().__del__()
 
     def draw(self, draw_attrs):
-
         self.vertices = [0, 0] + self.distribute(0.8, 0.8, draw_attrs.get("faces"))
         self.indices = self.tri_fan_indices(draw_attrs.get("faces"))
 
@@ -191,8 +228,7 @@ class RegPoly(Object):
 class Circle(RegPoly):
     def __init__(self, name, attributes):
         super().__init__(name, attributes)
-        self.default_attrs["faces"][0] = 40
-
+        self.default_attrs["faces"][0] = self.default_attrs["max_faces"][0]  # At faces > 20 looks like a circle enough
 
 
 class Scene:
@@ -209,9 +245,6 @@ class Scene:
         self.obj_children.append(child)
 
     def render(self, attributes, objects, scenes, shared_data, parent_attrs=None):
-
-        # todo restructure to move shaders and stuff into the shared data, and are now part of the expected values
-
         # Calculate inheritables
         parent_attrs = {} if parent_attrs is None else parent_attrs
         inheritables = parse_attribute_functions(self.self_attrs, attributes, shared_data)
@@ -227,9 +260,7 @@ class Scene:
         for name, scene_attrs in self.sc_children.items():
             if name not in scenes:
                 continue
-            scenes[name].render(attributes, objects, scenes, shared_data)
-
-        return 0
+            scenes[name].render(attributes, objects, scenes, shared_data)  # Incomplete, needs parent attributes to be parsed and passed
 
     def __repr__(self):
         return f"Scene {self.name}"
@@ -244,6 +275,8 @@ class Project:
         self.setup = {}
         self.shared_data = {}
         self.object_types = {"rect": RectObject, "regpoly": RegPoly, "circle": Circle}
+
+        self.background_color = [0, 0, 0]
 
     def update_object_types(self, types):
         self.object_types.update(types)
@@ -262,12 +295,20 @@ class Project:
         self.setup = {}
         with open(file_name) as file:
             data = yaml.load(file, Loader=yaml.FullLoader)
+
+            # Handy contractions
             scenes = data["graph"]["scenes"]
             objects = data["graph"]["objects"]
+
+            # Save to project memory
             self.shared_data = data["shared_data"]
             self.setup = data["setup"]
+
+            # Process object and scene dictionaries
             self.objects = {name: self.object_types.get(obj["type"])(name, obj) for name, obj in objects.items()}
             self.scenes = {name: Scene(name, sc["scenes"], sc["objects"], sc["self"]) for name, sc in scenes.items()}
+
+        self.background_color = self.shared_data.get("background", [0, 0, 0])
 
 
 def main():
@@ -288,11 +329,7 @@ def main():
     key_press_timers = [0, 0]
 
     if not screen.screen_is_valid():
-        raise RuntimeError("WadSDSD")
-
-    tex1 = gl.Texture("container.jpg", gl.Texture.Color_modes.rgb)
-
-    tex2 = gl.Texture("awesomeface.png", gl.Texture.Color_modes.rgba)
+        raise RuntimeError("Screen was not initialised properly")
 
     debugShader = gl.Shader("default.vert", "default.frag")
 
@@ -304,29 +341,38 @@ def main():
     last_time = 0.0
     mouseBound = False
     lastMouse = gl.Screen.PressModes.release
+    lastspace = False
 
     while not screen.should_close():
+        # Process time
         current_time = screen.get_time()
         delta_time = current_time - last_time
         last_time = current_time
 
         screen.poll()
 
+        # Process mouse movement
         xoffset = screen.pos_x - lastX
         yoffset = lastY - screen.pos_y  # reversed since y - coordinates range from bottom to top
         lastX = screen.pos_x
         lastY = screen.pos_y
 
+        # Legacy mouse binding
         if lastMouse == gl.Screen.PressModes.release:
             if screen.get_mouse_state(gl.Screen.MouseButtons.right) == gl.Screen.PressModes.press:
                 mouseBound = not mouseBound
 
         lastMouse = screen.get_mouse_state(gl.Screen.MouseButtons.right)
 
+        # Assorted key checking
         if screen.get_key_state(gl.Screen.Keys.x) == gl.Screen.PressModes.press:
             screen.set_should_close(True)
         if screen.get_key_state(gl.Screen.Keys.Space) == gl.Screen.PressModes.press:
-            step_mode = "play" if step_mode == "pause" else "pause"  ## Fix only rising edge
+            if lastspace:
+                step_mode = "play" if step_mode == "pause" else "pause"  ## Fix only rising edge
+            lastspace = False
+        else:
+            lastspace = True
         if screen.get_key_state(gl.Screen.Keys.Left) == gl.Screen.PressModes.press:
             elapsed_frames -= 1
             key_press_timers[0] = current_time
@@ -343,14 +389,16 @@ def main():
 
         aspect = size_y / size_x
 
+        background_color = project.background_color
+
         # -------------------------------
-        screen.set_color(0.2, 0.3, 0.3, 1)
+
+        screen.set_color(*background_color, 1)
         screen.clear(True, True)
 
         debugShader.use()
         project.render({"debug_draw_bounds": True, "frames": elapsed_frames, "shader": debugShader, "screen_x": size_x,
                         "screen_y": size_y, "aspect": aspect})
-
         screen.flip()
 
         if step_mode == "play":
@@ -361,11 +409,5 @@ def main():
     screen.stop()
     return 0
 
-    # gl.main(screen)
-
 
 main()
-
-'''for l in [cubePositions[i:i + 3] for i in range(0, len(cubePositions), 3)]:
-        l = [str(q) for q in l]
-        print(", ".join(l) + ",") '''
